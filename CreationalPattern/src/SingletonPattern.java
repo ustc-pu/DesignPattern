60fbe353694d555e65ca70b0fad7fd6695988366
pcheng19 <pacheng19@gmail.com>
2024-01-20 10:56:44 -0500

    singleton pattern

---
+++ CreationalPattern/src/SingletonPattern.java
@@ -0,0 +1,33 @@
+public class SingletonPattern {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        singleton1.display();
+        System.out.println(singleton1);
+
+        Singleton singleton2 = Singleton.getInstance();
+        singleton2.display();
+        System.out.println(singleton2);
+
+        // True
+        System.out.println(singleton2 == singleton1);
+    }
+}
+
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void display() {
+        System.out.println("This is a singleton object.");
+    }
+}
